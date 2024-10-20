67dce84743a2a7f0034fa1889bbf4d611d25a54b
Ravichandran7 <ravichandranruthramoorthy@gmail.com>
2024-10-20 23:33:10 +0530

    up to register & login

--- myProject/myApp/form.py
+++ myProject/myApp/form.py
@@ -4,6 +4,12 @@ from .models import User
 
 class CustomUserForm(UserCreationForm):
     username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter User Name'}))
+    email=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter User Email Address'}))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter User Password'}))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter User Conform Password'}))
+
+
+
     class Meta:
         model=User
         fields=['username','email','password1','password2' ]
--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("",views.home,name="home"),
     path("register",views.register,name="register"),
+    path("login",views.login_page,name="login"),
+    path('logout',views.logout_page,name="logout"),
     path("Courses",views.courses,name="Courses"),
     path('courses/<str:name>/', views.courses_view, name='courses'),
     path('courses/<str:cname>/<str:pname>', views.courses_details, name='courses_details'),
