78cf0a561927b4117485fc63f924311317020b8e
Ravichandran7 <ravichandranruthramoorthy@gmail.com>
2024-10-20 17:52:17 +0530

    up to course details views

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -15,12 +15,22 @@ def courses(request):
 def courses_view(request, name):
     try:
         # Ensure you fetch a single category, since filter returns a queryset
-        category = Catagory.objects.get(name=name, status=0)  # use get() for single category
+        Catagory.objects.get(name=name, status=0)  # use get() for single category
         # Now filter courses by the related category using the correct field name
         courses = Courses.objects.filter(Catagory__name=name)
         return render(request, "myApp/products/index.html", {"courses": courses, "catagory__name": name})
     except Catagory.DoesNotExist:  # Handle the case where the category doesn't exist
         messages.warning(request, "NO SUCH COURSES FOUND")
         return redirect('courses')
+    
 def courses_details(request,cname,pname):
-    return redirect("Courses")
+    if(Catagory.objects.filter(name=cname,status=0)):
+        if(Courses.objects.filter(name=pname,status=0)):
+            courses=Courses.objects.filter(name=pname,status=0).first()
+            return render(request, "myApp/products/courses_details.html", {"courses": courses})
+        else:
+            messages.error(request, "NO SUCH COURSES FOUND")
+            return redirect('courses')
+    else:
+        messages.error(request, "NO SUCH COURSES FOUND")
+        return redirect('courses')
